7ae933ec38f56e09876b9e7bb69f1d65febfac6a
karimegaber <karime.gaber@gmail.com>
2022-07-01 15:04:23 +0200

    Implemented ErrorHandler class

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -142,3 +142,8 @@ extension DataSourceExtension on DataSource {
     }
   }
 }
+
+class ApiInternalStatus {
+  static const int SUCCESS = 0;
+  static const int FAILURE = 1;
+}
--- lib/data/repository/repository_Implementer.dart
+++ lib/data/repository/repository_Implementer.dart
@@ -1,5 +1,6 @@
 import 'package:advanced_app/data/data_source/remote_data_source.dart';
 import 'package:advanced_app/data/mapper/mapper.dart';
+import 'package:advanced_app/data/network/error_handler.dart';
 import 'package:advanced_app/data/network/failure.dart';
 import 'package:advanced_app/data/network/network_info.dart';
 import 'package:advanced_app/data/network/requests.dart';
@@ -18,20 +19,27 @@ class RepositoryImplementer implements Repository {
       LoginRequest loginRequest) async {
     // Internet is connected.
     if (await _networkInfo.isConnected) {
-      final response = await _remoteDataSource.login(loginRequest);
+      try {
+        final response = await _remoteDataSource.login(loginRequest);
 
-      if (response.status == 0) {
-        //Success
-        return Right(response.toDomain());
-      } else {
-        //Failure
-        return Left(Failure(
-            response.status ?? 400, response.message ?? 'An Error happened!'));
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          //Success
+          return Right(response.toDomain());
+        } else {
+          //Failure
+          return Left(Failure(
+            ApiInternalStatus.FAILURE,
+            response.message ?? ResponseMessage.DEFAULT,
+          ));
+        }
+      } catch (error) {
+        // if an error happened.
+        return Left(ErrorHandler.handle(error).failure);
       }
     }
     // Internet is NOT connected.
     else {
-      return Left(Failure(400, 'An Error happened!'));
+      return Left(DataSource.NOT_INTERNET_CONNECTION.getFailure());
     }
   }
 }
